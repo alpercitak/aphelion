33445283e071a63ea9a7fd57bfc4901c589a4a07
Alper Çıtak <citak.alper@gmail.com>
2026-04-14 08:57:31 +0200

    black-hole: update hook

---
+++ src/features/black-hole/hooks/update.ts
@@ -0,0 +1,81 @@
+import { useEffect } from 'react';
+import { Mesh } from 'three';
+import { applyMassScale } from '../utils/mass-scale';
+import { createAccretionDisk } from '../utils/accretion-disk';
+import type { SceneRef, Params } from '../types';
+import type { SceneRefType } from '@/types';
+
+export const useUpdate = (sceneRef: SceneRefType<SceneRef>, params: Params) => {
+  useEffect(() => {
+    const refs = sceneRef.current;
+    if (!refs) {
+      return;
+    }
+    const { entities } = refs;
+    const { blackHole, photonSphere, outerGlow, photonRing, einsteinRing } = entities;
+    const { mass, spin } = params;
+    applyMassScale({ blackHole, photonSphere, outerGlow, photonRing, einsteinRing }, mass, spin);
+  }, [params.mass, params.spin, sceneRef]);
+
+  useEffect(() => {
+    const refs = sceneRef.current;
+    if (!refs) {
+      return;
+    }
+    const { core, entities } = refs;
+
+    // cleanup old disk
+    core.scene.remove(entities.diskGroup);
+    entities.diskGroup.traverse((o) => {
+      if (o instanceof Mesh) {
+        o.geometry?.dispose();
+        o.material?.dispose();
+      }
+    });
+
+    // Create and swap
+    const newDisk = createAccretionDisk(params.temp, params.dopplerShift);
+    entities.diskGroup = newDisk;
+
+    if (params.showDisk) {
+      core.scene.add(newDisk);
+    }
+  }, [params.temp, params.dopplerShift, params.showDisk, sceneRef]);
+
+  useEffect(() => {
+    const refs = sceneRef.current;
+    if (!refs) {
+      return;
+    }
+    const { core, entities } = refs;
+    const { stars, scene } = core;
+    const { jetsGroup, diskGroup } = entities;
+
+    stars.visible = params.showStars;
+
+    if (params.showJets) {
+      scene.add(jetsGroup);
+    } else {
+      scene.remove(jetsGroup);
+    }
+
+    if (params.showDisk) {
+      scene.add(diskGroup);
+    } else {
+      scene.remove(diskGroup);
+    }
+  }, [params.showStars, params.showJets, params.showDisk, sceneRef]);
+
+  useEffect(() => {
+    const refs = sceneRef.current;
+    if (!refs) {
+      return;
+    }
+    const { entities } = refs;
+    const { photonMat, photonRingMat, einsteinMat } = entities;
+
+    photonRingMat.opacity = params.lensStrength * 0.9;
+    einsteinMat.opacity = params.lensStrength * 0.5;
+    photonMat.uniforms.glowColor?.value.setRGB(1.0, 0.67 * params.lensStrength, 0.27 * params.lensStrength);
+  }, [params.lensStrength, sceneRef]);
+};
--- src/features/black-hole/index.tsx
+++ src/features/black-hole/index.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useRef } from 'react';
-import { Mesh } from 'three';
-
+import { useRef } from 'react';
 import SceneLayout from '@/components/app/scene-layout';
-
 import { useAnimate } from './hooks/animate';
 import { useControls } from './hooks/controls';
 import { useHud } from './hooks/hud';
 import { useInit } from './hooks/init';
+import { useUpdate } from './hooks/update';
 import type { SceneRef } from './types';
-import { createAccretionDisk } from './utils/accretion-disk';
-import { applyMassScale } from './utils/mass-scale';
 
 export default function BlackHole() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -19,85 +15,8 @@ export default function BlackHole() {
   const hud = useHud(params);
 
   useInit(canvasRef, sceneRef);
+  useUpdate(sceneRef, params);
   useAnimate(sceneRef, paramsRef);
-
-  // ── React to param changes ────────────────────────────────────────────────
-  useEffect(() => {
-    const refs = sceneRef.current;
-    if (!refs) {
-      return;
-    }
-    const { entities } = refs;
-    const { blackHole, photonSphere, outerGlow, photonRing, einsteinRing } = entities;
-    applyMassScale({ blackHole, photonSphere, outerGlow, photonRing, einsteinRing }, params.mass, params.spin);
-  }, [params.mass, params.spin]);
-
-  useEffect(() => {
-    const refs = sceneRef.current;
-    if (!refs) {
-      return;
-    }
-    const { core, entities } = refs;
-    core.scene.remove(entities.diskGroup);
-    entities.diskGroup.traverse((o) => {
-      if (o instanceof Mesh) {
-        o.geometry?.dispose();
-        o.material?.dispose();
-      }
-    });
-    const newDisk = createAccretionDisk(params.temp, params.dopplerShift);
-    entities.diskGroup = newDisk;
-    if (sceneRef.current) {
-      sceneRef.current.entities.diskGroup = newDisk;
-    }
-    if (params.showDisk) {
-      refs.core.scene.add(newDisk);
-    }
-  }, [params.temp, params.dopplerShift]);
-
-  useEffect(() => {
-    const refs = sceneRef.current;
-    if (!refs) {
-      return;
-    }
-    const { core, entities } = refs;
-    if (params.showDisk) {
-      core.scene.add(entities.diskGroup);
-    } else {
-      core.scene.remove(entities.diskGroup);
-    }
-  }, [params.showDisk]);
-
-  useEffect(() => {
-    const refs = sceneRef.current;
-    if (!refs) {
-      return;
-    }
-    if (params.showJets) {
-      refs.core.scene.add(refs.entities.jetsGroup);
-    } else {
-      refs.core.scene.remove(refs.entities.jetsGroup);
-    }
-  }, [params.showJets]);
-
-  useEffect(() => {
-    const refs = sceneRef.current;
-    if (!refs) {
-      return;
-    }
-    refs.core.stars.visible = params.showStars;
-  }, [params.showStars]);
-
-  useEffect(() => {
-    const refs = sceneRef.current;
-    if (!refs) {
-      return;
-    }
-    const { entities } = refs;
-    entities.photonRingMat.opacity = params.lensStrength * 0.9;
-    entities.einsteinMat.opacity = params.lensStrength * 0.5;
-    entities.photonMat.uniforms.glowColor?.value.setRGB(1.0, 0.67 * params.lensStrength, 0.27 * params.lensStrength);
-  }, [params.lensStrength]);
 
   return <SceneLayout canvasRef={canvasRef} hud={hud} controls={controls} />;
 }
